2aad1c24561c599f7730d57969d4cf8720209662
Matthew Miller <35173022+Esjihn@users.noreply.github.com>
2020-05-29 15:42:52 -0400

    Add project files.

---
+++ RandomNumberService/RandomNumberGeneratorService.svc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr.Runtime;
+using RandomNumberService.Models;
+
+namespace RandomNumberService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RandomNumberGeneratorService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RandomNumberGeneratorService.svc or RandomNumberGeneratorService.svc.cs at the Solution Explorer and start debugging.
+    public class RandomNumberGeneratorService : IRandomNumberGeneratorService
+    {
+        public NumberModel GetRandomNumbers()
+        {
+            Random random = new Random();
+
+            NumberModel numberObject = new NumberModel
+            {
+                RandomNumber1 = random.Next(-500, 5000),
+                RandomNumber2 = random.Next(-500, 5000),
+                RandomNumber3 = random.Next(-500, 5000),
+            };
+            return numberObject;
+        }
+    }
+}
---
+++ RandomNumberWinForms/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RandomNumberService;
+using RandomNumberService.Models;
+
+namespace RandomNumberWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RandomNumberGeneratorService service = new RandomNumberGeneratorService();
+
+            var numbers = service.GetRandomNumbers();
+            List<string> numbersList = new List<string>();
+
+            numbersList.Add(numbers.RandomNumber1.ToString());
+            numbersList.Add(numbers.RandomNumber2.ToString());
+            numbersList.Add(numbers.RandomNumber3.ToString());
+
+            this.richTextBox1.Clear();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string item in numbersList)
+            {
+                sb.Append(item + " ");
+            }
+
+            richTextBox1.Text = sb.ToString();
+
+        }
+    }
+}
